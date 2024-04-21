19220926c68a7dbbed20382d71326acd4fbd5202
arjunQ21 <arjunq21@gmail.com>
2024-04-21 21:27:51 +0545

    Appending now

--- splitter.py
+++ splitter.py
@@ -1,38 +1,44 @@
 from os import path
 
-file_size_limit = 1 * 1024 * 1024 # 1 MB max .mbox file size
-mbox_file_path = 'All mail Including Spam and Trash.mbox'  # Change this to your .mbox file path
+file_size_limit = 220 * 1024 * 1024 # 1 MB max .mbox file size
+mbox_file_path = '/Users/arjunq21/Downloads/Takeout-acc/Mail/All mail Including Spam and Trash.mbox'  # Change this to your .mbox file path
 destination ="splitted"
 
-count = 0
 part_no = 0
+count = 0
 current_file_content = ""
+len_current_file_content = 0 
 current_email_content = ""
+len_current_email_content = 0
 to_save = ""
 
 def save():
-    global part_no, current_file_content
+    global part_no, current_file_content, len_current_file_content
     with open( path.join(destination,  f"part_{part_no}.mbox"), "w") as file:
         file.write(current_file_content)
         file.close()
     print(f"Saved part {part_no}")
     part_no += 1
     current_file_content = ""
+    len_current_file_content = 0
 
 with open(mbox_file_path, "r") as file:
     for line in file:
         if(line.startswith("From ")):
             count += 1
             print(f"Processing Email {count}")
-            if(len(current_email_content) > 0):
-                if(len(current_email_content) + len(current_file_content) > file_size_limit):
+            if(len_current_email_content > 0):
+                if(len_current_email_content + len_current_file_content > file_size_limit):
                     save()
                 else:
                     current_file_content += "\n" + current_email_content
+                    len_current_file_content += len("\n" + current_email_content)
                     current_email_content = ""
+                    len_current_email_content = 0
 
 
         current_email_content += line
+        len_current_email_content += len(line)
 
 current_file_content += "\n" + current_email_content
 current_email_content = ""
