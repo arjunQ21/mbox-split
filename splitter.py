f818fb46c0e7765101eadf6f1f3c2a6f9c158b50
arjunQ21 <arjunq21@gmail.com>
2024-04-22 05:32:15 +0545

    Made with appending logic, instead of saving, for performance optimization

--- splitter.py
+++ splitter.py
@@ -1,47 +1,40 @@
 from os import path
-
-file_size_limit = 220 * 1024 * 1024 # 1 MB max .mbox file size
-mbox_file_path = '/Users/arjunq21/Downloads/Takeout-acc/Mail/All mail Including Spam and Trash.mbox'  # Change this to your .mbox file path
+file_size_limit = 220 * 1024 * 1024 # 220 MB max .mbox file size
+mbox_file_path = 'All mail Including Spam and Trash.mbox'  # Change this to your .mbox file path
 destination ="splitted"
 
 part_no = 0
 count = 0
-current_file_content = ""
-len_current_file_content = 0 
 current_email_content = ""
-len_current_email_content = 0
 to_save = ""
 
-def save():
-    global part_no, current_file_content, len_current_file_content
-    with open( path.join(destination,  f"part_{part_no}.mbox"), "w") as file:
-        file.write(current_file_content)
+def append():
+    global part_no, count, current_email_content, file_size_limit
+    file_save_path = path.join(destination,  f"part_{part_no}.mbox")
+    file_size = 0
+    if(path.exists(file_save_path)):
+        file_size = path.getsize(file_save_path)
+    if(file_size + len(current_email_content) > file_size_limit):
+        part_no += 1
+        append()
+        return 
+    with open( file_save_path, "a") as file:
+        file.write(current_email_content)
         file.close()
-    print(f"Saved part {part_no}")
-    part_no += 1
-    current_file_content = ""
-    len_current_file_content = 0
+    print(f"Appended email #{count} in part:{part_no}")
+    current_email_content = ""
 
 with open(mbox_file_path, "r") as file:
     for line in file:
         if(line.startswith("From ")):
             count += 1
             print(f"Processing Email {count}")
-            if(len_current_email_content > 0):
-                if(len_current_email_content + len_current_file_content > file_size_limit):
-                    save()
-                else:
-                    current_file_content += "\n" + current_email_content
-                    len_current_file_content += len("\n" + current_email_content)
-                    current_email_content = ""
-                    len_current_email_content = 0
+            if(len(current_email_content) > 0):
+                append()
 
 
         current_email_content += line
-        len_current_email_content += len(line)
 
-current_file_content += "\n" + current_email_content
-current_email_content = ""
-save()
+append()
 
 
