460e3864f0dfb4303f0c4a8e79f25740627fac67
arjunQ21 <arjunq21@gmail.com>
2024-04-22 19:07:25 +0545

    Made very fast. Previously took 4-5 hours. Now does this in 30-40 seconds.

--- splitter.py
+++ splitter.py
@@ -1,38 +1,30 @@
 from os import path
-file_size_limit = 220 * 1024 * 1024 # 220 MB max .mbox file size
-mbox_file_path = '/Users/arjunq21/Downloads/All mail Including Spam and Trash-002.mbox'  # Change this to your .mbox file path
-destination ="splitted-rajendra"
-
+file_size_limit = 190 * 1024 * 1024 # 190 MB (+ Last Email's Size) max .mbox file size in output .mbox files 
+mbox_file_path = '/Users/arjunq21/Downloads/All mail Including Spam and Trash-002 (1).mbox'  # Change this to your .mbox file path
+destination ="splitted"
 part_no = 0
 count = 0
-current_email_content = ""
-to_save = ""
 
-def append():
-    global part_no, count, current_email_content, file_size_limit
-    file_save_path = path.join(destination,  f"part_{part_no}.mbox")
-    file_size = 0
-    if(path.exists(file_save_path)):
-        file_size = path.getsize(file_save_path)
-    if(file_size + len(current_email_content) > file_size_limit):
-        part_no += 1
-        append()
-        return 
-    with open( file_save_path, "a") as file:
-        file.write(current_email_content)
-        file.close()
-    print(f"Appended email #{count} in part:{part_no}")
-    current_email_content = ""
+def get_file_save_path():
+    global part_no, destination
+    return path.join(destination, f"new_part_{part_no}.mbox")
+
+current_file = open(get_file_save_path(), "a")
 
 with open(mbox_file_path, "r") as file:
     for line in file:
         if(line.startswith("From ")):
             count += 1
             print(f"Processing Email {count}")
-            if(len(current_email_content) > 0):
-                append()
+            if(path.getsize(get_file_save_path()) > file_size_limit):
+                current_file.close()
+                part_no += 1
+                current_file = open(get_file_save_path(), "a")
+                print(f"Created New File: {get_file_save_path()}")
+            
+        current_file.write(line)
 
 
-        current_email_content += line
+
 
 
